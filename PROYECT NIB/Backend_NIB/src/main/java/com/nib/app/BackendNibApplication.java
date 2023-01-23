760c2362fb9d8488e68a9ad14cc78de52c465e61
Clara Bujeda <95882405+CBujeda@users.noreply.github.com>
2023-01-23 09:34:19 +0100

    Main Backend

---
+++ PROYECT NIB/Backend_NIB/src/main/java/com/nib/app/BackendNibApplication.java
@@ -0,0 +1,13 @@
+package com.nib.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNibApplication.class, args);
+	}
+
+}
---
+++ PROYECT NIB/Backend_NIB/src/test/java/com/nib/app/BackendNibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nib.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
