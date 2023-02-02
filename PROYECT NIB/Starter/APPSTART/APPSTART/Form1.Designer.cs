949d2571ef1fdc317021ff829beb74eea8f5960c
Clara Bujeda <95882405+CBujeda@users.noreply.github.com>
2023-02-02 16:49:39 +0100

    botones y estilos

--- PROYECT NIB/Starter/APPSTART/APPSTART/Form1.Designer.cs
+++ PROYECT NIB/Starter/APPSTART/APPSTART/Form1.Designer.cs
@@ -33,6 +33,11 @@ namespace APPSTART
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.StartBack = new System.Windows.Forms.Button();
+            this.front_server = new System.Windows.Forms.Button();
+            this.Config_gen = new System.Windows.Forms.Button();
+            this.start_all = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // label1
@@ -47,7 +52,7 @@ namespace APPSTART
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Location = new System.Drawing.Point(12, 104);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 20);
             this.label2.TabIndex = 1;
@@ -56,7 +61,7 @@ namespace APPSTART
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 116);
+            this.label3.Location = new System.Drawing.Point(12, 136);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 20);
             this.label3.TabIndex = 2;
@@ -72,15 +77,64 @@ namespace APPSTART
             this.StartBack.UseVisualStyleBackColor = true;
             this.StartBack.Click += new System.EventHandler(this.StartBack_Click);
             // 
+            // front_server
+            // 
+            this.front_server.Location = new System.Drawing.Point(124, 100);
+            this.front_server.Name = "front_server";
+            this.front_server.Size = new System.Drawing.Size(94, 29);
+            this.front_server.TabIndex = 4;
+            this.front_server.Text = "NONE";
+            this.front_server.UseVisualStyleBackColor = true;
+            // 
+            // Config_gen
+            // 
+            this.Config_gen.Location = new System.Drawing.Point(124, 136);
+            this.Config_gen.Name = "Config_gen";
+            this.Config_gen.Size = new System.Drawing.Size(94, 29);
+            this.Config_gen.TabIndex = 5;
+            this.Config_gen.Text = "NONE";
+            this.Config_gen.UseVisualStyleBackColor = true;
+            // 
+            // start_all
+            // 
+            this.start_all.Location = new System.Drawing.Point(606, 64);
+            this.start_all.Name = "start_all";
+            this.start_all.Size = new System.Drawing.Size(94, 29);
+            this.start_all.TabIndex = 6;
+            this.start_all.Text = "NONE";
+            this.start_all.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(488, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "START ALL";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(3, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(252, 148);
+            this.panel1.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.start_all);
+            this.Controls.Add(this.Config_gen);
+            this.Controls.Add(this.front_server);
             this.Controls.Add(this.StartBack);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -94,6 +148,11 @@ namespace APPSTART
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button StartBack;
+        private System.Windows.Forms.Button front_server;
+        private System.Windows.Forms.Button Config_gen;
+        private System.Windows.Forms.Button start_all;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
