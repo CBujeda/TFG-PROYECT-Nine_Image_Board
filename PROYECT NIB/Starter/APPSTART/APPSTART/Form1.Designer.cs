e9985d2650fab9ef4085363bc83a84acdffa7631
Clara Bujeda <95882405+CBujeda@users.noreply.github.com>
2023-02-02 18:58:06 +0100

    APPStart buttons and shell

--- PROYECT NIB/Starter/APPSTART/APPSTART/Form1.Designer.cs
+++ PROYECT NIB/Starter/APPSTART/APPSTART/Form1.Designer.cs
@@ -38,6 +38,11 @@ namespace APPSTART
             this.start_all = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.shell = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -85,6 +90,7 @@ namespace APPSTART
             this.front_server.TabIndex = 4;
             this.front_server.Text = "NONE";
             this.front_server.UseVisualStyleBackColor = true;
+            this.front_server.Click += new System.EventHandler(this.front_server_Click);
             // 
             // Config_gen
             // 
@@ -94,10 +100,11 @@ namespace APPSTART
             this.Config_gen.TabIndex = 5;
             this.Config_gen.Text = "NONE";
             this.Config_gen.UseVisualStyleBackColor = true;
+            this.Config_gen.Click += new System.EventHandler(this.Config_gen_Click);
             // 
             // start_all
             // 
-            this.start_all.Location = new System.Drawing.Point(606, 64);
+            this.start_all.Location = new System.Drawing.Point(345, 19);
             this.start_all.Name = "start_all";
             this.start_all.Size = new System.Drawing.Size(94, 29);
             this.start_all.TabIndex = 6;
@@ -107,7 +114,7 @@ namespace APPSTART
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(488, 68);
+            this.label4.Location = new System.Drawing.Point(261, 23);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 20);
             this.label4.TabIndex = 7;
@@ -115,17 +122,50 @@ namespace APPSTART
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel1.Location = new System.Drawing.Point(3, 46);
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(3, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(252, 148);
+            this.panel1.Size = new System.Drawing.Size(252, 163);
             this.panel1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(3, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 38);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "SYSTEMS";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.shell);
+            this.panel2.Location = new System.Drawing.Point(3, 228);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(471, 318);
+            this.panel2.TabIndex = 9;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // shell
+            // 
+            this.shell.AutoSize = true;
+            this.shell.ForeColor = System.Drawing.Color.Yellow;
+            this.shell.Location = new System.Drawing.Point(4, 4);
+            this.shell.Name = "shell";
+            this.shell.Size = new System.Drawing.Size(0, 20);
+            this.shell.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(479, 565);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.start_all);
             this.Controls.Add(this.Config_gen);
@@ -136,7 +176,11 @@ namespace APPSTART
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "NIB - CONTROLLER";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,6 +197,9 @@ namespace APPSTART
         private System.Windows.Forms.Button start_all;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label shell;
     }
 }
 
--- PROYECT NIB/Starter/APPSTART/APPSTART/Form1.cs
+++ PROYECT NIB/Starter/APPSTART/APPSTART/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,25 +13,55 @@ namespace APPSTART
 {
     public partial class Form1 : Form
     {
-        private bool var_startback;
         public Form1()
         {
             InitializeComponent();
             StartBack.Text = "START";
-            var_startback = false;
+            front_server.Text = "START";
         }
 
         private void StartBack_Click(object sender, EventArgs e)
+        {  
+            StartBack.Text = "STARTING...";
+            StartBack.Enabled = false;
+            StartBack.BackColor = Color.Yellow;
+            StartBack.ForeColor = Color.Black;
+            shell.Text = shell.Text + "Click StartBack \n";
+            StartBack.BackColor = Color.GreenYellow;
+        }
+
+        private void front_server_Click(object sender, EventArgs e)
         {
-            if (var_startback == true)
-            {
-                var_startback = false;
-                StartBack.Text = "START";
+            front_server.Text = "STARTING...";
+            front_server.Enabled = false;
+            front_server.BackColor = Color.Yellow;
+            front_server.ForeColor = Color.Black;
+            shell.Text = shell.Text + "Click Front \n";
+            front_server.BackColor = Color.GreenYellow;
+        }
+
+        private void Config_gen_Click(object sender, EventArgs e)
+        {
+            Config_gen.Text = "STARTING...";
+            Config_gen.Enabled = false;
+            Config_gen.BackColor = Color.Yellow;
+            Config_gen.ForeColor = Color.Black;
+            for (int i = 0; i < 50; i++) {
+                addShell(i+"");
+
+
             }
-            else {
-                var_startback = true;
-                StartBack.Text = "STOP";
-            }
+            Config_gen.BackColor = Color.GreenYellow;
+        }
+
+        private void addShell(String txt) { addShell("INFO",txt); }
+        private void addShell(String type,String txt) {
+            shell.Text = "["+type+"] -> " + txt + " \n" + shell.Text;
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
