118abeac6c811708073f0d1e46bea7e943aa5fb1
Clara Bujeda <95882405+CBujeda@users.noreply.github.com>
2023-02-03 17:09:20 +0100

    Auto Configurator proyect v1.0.1

---
+++ configurator.bat
@@ -0,0 +1,29 @@
+title starter
+Set _fGreen=[32m
+Set _fYellow=[33m
+Set _fBRed=[91m
+@echo off
+cls
+echo %_fGreen%
+echo.                          
+echo          ,--.                   
+echo        ,--.'^|   ,---,    ,---,. 
+echo    ,--,:  : ^|,`--.' ^|  ,'  .'  \
+echo ,`--.'`^|  ' :^|   :  :,---.' .' ^|
+echo ^|   :  :  ^| ^|:   ^|  '^|   ^|  ^|: ^|
+echo :   ^|   \ ^| :^|   :  ^|:   :  :  /
+echo ^|   : '  '; ^|'   '  ;:   ^|    ; 
+echo '   ' ;.    ;^|   ^|  ^|^|   :     \
+echo ^|   ^| ^| \   ^|'   :  ;^|   ^|   . ^|
+echo '   : ^|  ; .'^|   ^|  ''   :  '; ^|
+echo ^|   ^| '`--'  '   :  ^|^|   ^|  ^| ; 
+echo '   : ^|      ;   ^|.' ^|   :   /     
+echo ;   ^|.'      '---'   ^|   ^| ,'   %_fYellow%		(v1.0.1) %_fGreen%
+echo '---'                `----'     
+echo 	%_fBRed%APP INIT
+echo %_fYellow%
+echo Generando Resources....
+cd ./PROYECT NIB/Compiled
+java -jar NIB_CONFIGURATOR.jar
+
+exit
