85588e5f56b11e1c5e3faf44dee31cef824d7b23
Clara Bujeda <95882405+CBujeda@users.noreply.github.com>
2023-04-10 10:56:44 +0200

    upload animator

--- PROYECT NIB/FrontEnd_NIB/src/app/pages/public-home/public-home.component.ts
+++ PROYECT NIB/FrontEnd_NIB/src/app/pages/public-home/public-home.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-
+import { backserv } from '../../../../config';
+import axios from "axios";
+import { PostsService } from 'src/app/services/posts.service';
+import { ConfAPPService } from 'src/app/services/conf-app.service';
 @Component({
   selector: 'app-public-home',
   templateUrl: './public-home.component.html',
@@ -12,70 +15,101 @@ export class PublicHomeComponent {
   predictedList: String[] = [];
 
   public contList: String[] = [];
-  public contbasepath ="../../../assets/contador/"
-  public numberCont = 0;
+  public contbasepath = "../../../assets/contador/"
+  public numberCont = "0";
+  public recomendedList: String[] = [];
+  public nameAPP = "";
 
   constructor(
-    public translate: TranslateService
+    public translate: TranslateService,
+    private postservice: PostsService,
+    private confappsevice: ConfAPPService
   ) {
     this.setConList();
+    this.setNameApp();
     this.placeholderAnimator();
 
   }
 
-  private setConList(){
-    let numbers = String(this.numberCont);
-    for(let i = 0; i < numbers.length;i++){
-      this.contList.push(numbers[i]+".gif")
-    }
+  private setConList() {
+
+    this.postservice.getContPosts().subscribe(
+      data => {
+        let numbers = String(data);
+        console.log(numbers);
+        this.numberCont = numbers;
+        for (let i = 0; i < numbers.length; i++) {
+          this.contList.push(numbers[i] + ".gif")
+        }
+      },
+      err =>{
+        console.log(err.error());
+        this.contList.push("E.gif")
+      }
+    );
+  }
+
+  public setNameApp(){
+    this.confappsevice.getNameAPP().subscribe(
+      data => {
+        console.log(data)
+        let nombre = String(data);
+        console.log(nombre)
+        this.nameAPP = nombre;
+      },
+      err =>{
+        console.log(err.error());
+      }
+    );
   }
 
 
-  public onChangeValue(event: String){
+  public onChangeValue(event: String) {
 
   }
 
-
-
-  private addWord(wordPredict:String) {
+  private addWord(wordPredict: String) {
     this.predictedList.push(""
       + "<a href = \"#\" class= \"text-decoration-none \" >"
-      + "<li class=\"list-group-item\" > "+wordPredict+" </li>"
+      + "<li class=\"list-group-item\" > " + wordPredict + " </li>"
       + " </a>"
-      +"")
+      + "")
   }
 
   private async placeholderAnimator() {
-  let values = ["girl", "bird", "boy", "bonito", "beautiful"];
-  while (true) {
-    for (let i = 0; i < values.length; i++) {
-      let tempHolder = "";
-      for (let x = 0; x < values[i].length; x++) {
-        tempHolder = tempHolder + values[i][x]
-        this.animatedPlaceholder = tempHolder;
-        await this.delay(200);
+    let uCont = "http://" + backserv.ipnibbackserver + ":" + backserv.portnibbackserver + "/api/public/tags/random10"
+    const response = await axios.get(uCont);
+    let array = await response.data;
+    if(array.length > 0){
+      for(let i = 0; i < array.length; i ++){
+        console.log(array[i].name)
+        let a = String(array[i].name)
+        this.recomendedList.push(a)
       }
-      for (let x = 0; x < 3; x++) {
-        tempHolder = tempHolder + "."
-        this.animatedPlaceholder = tempHolder;
-        await this.delay(100);
+    }
+    while (true) {
+      for (let i = 0; i < this.recomendedList.length; i++) {
+        let tempHolder = "";
+        for (let x = 0; x < this.recomendedList[i].length; x++) {
+          tempHolder = tempHolder + this.recomendedList[i][x]
+          this.animatedPlaceholder = tempHolder;
+          await this.delay(200);
+        }
+        for (let x = 0; x < 3; x++) {
+          tempHolder = tempHolder + "."
+          this.animatedPlaceholder = tempHolder;
+          await this.delay(100);
+        }
+        await this.delay(1000);
       }
-      await this.delay(1000);
     }
   }
-}
 
   private async sleep(ms: number) {
-  //Say Hello
-  console.log('Hello');
-  // Say World after 2000 milliseconds
-  await this.delay(ms);
-  console.log('Hello2');
-}
-
+    await this.delay(ms);
+  }
 
   private delay(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 }
