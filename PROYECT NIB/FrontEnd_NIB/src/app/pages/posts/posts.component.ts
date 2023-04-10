f4c59a7be1e831dadb1483440c1bcbeae145484f
Clara Bujeda <95882405+CBujeda@users.noreply.github.com>
2023-04-10 10:58:47 +0200

    posts page

--- PROYECT NIB/FrontEnd_NIB/src/app/pages/posts/posts.component.ts
+++ PROYECT NIB/FrontEnd_NIB/src/app/pages/posts/posts.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
+  selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent {
 
-  public posts: String[] = ["","","","","","","","","","","","","","","",""];
+  public posts: String[] = [];
+  public page = 0;
+  public size = 18;
+  public order = "date";
+  public asc = true;
+
+
+  constructor(private postservice:PostsService){
+    this.cargar_posts();
+    for(let i = 0; i < 20; i++){
+      this.posts.push("");
+    }
+
+  }
+
+  cargar_posts(){
+    this.postservice.postsPages(this.page,this.size,this.order,this.asc).subscribe(
+      data => {
+        console.log(data);
+      },
+      err =>{
+        console.log(err.error());
+      }
+    );
+  }
 
 }
