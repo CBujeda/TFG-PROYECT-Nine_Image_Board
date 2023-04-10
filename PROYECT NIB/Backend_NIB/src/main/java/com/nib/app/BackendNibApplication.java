42481df90767f0a40176de51d0bfdc3ce8de1fda
Clara Bujeda <95882405+CBujeda@users.noreply.github.com>
2023-04-10 10:52:36 +0200

    Sistema de auto generate data in BBDD

--- PROYECT NIB/Backend_NIB/src/main/java/com/nib/app/BackendNibApplication.java
+++ PROYECT NIB/Backend_NIB/src/main/java/com/nib/app/BackendNibApplication.java
@@ -9,8 +9,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nib.app.model.entity.Config;
 import com.nib.app.model.entity.user.Rol;
 import com.nib.app.model.entity.user.Usuario;
+import com.nib.app.model.service.ConfService;
 import com.nib.app.model.service.RolService;
 import com.nib.app.model.service.UsuarioService;
 
@@ -25,6 +27,9 @@ public class BackendNibApplication implements CommandLineRunner {
 	@Autowired
 	private RolService rolService;
 	
+	@Autowired
+	private ConfService confserv;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendNibApplication.class, args);
@@ -33,19 +38,29 @@ public class BackendNibApplication implements CommandLineRunner {
 	@Transactional
 	@Override
 	public void run(String... args) throws Exception {
-		Rol r = new Rol();
-		r.setId_rol(1L);
-		r.setNombre("ADMIN"); 		
-		addRol(r);
+		printInfo("Verifying configuration...");
+		if(confserv.getCont() == 0) {
+			printInfo("Updating configuration");
+			confserv.addConf(new Config("app_name","Nine Image Board"));
+		}
 		
-		Rol r2 = new Rol();
-		r2.setId_rol(2L);
-		r2.setNombre("USER"); 
-		addRol(r2);
 		
+		String username = "ADMIN";
 		Usuario usuario = new Usuario();
 		usuario.setUsername("ADMIN");
+
+		printInfo("Verifying Users");
 		if(!usuarioService.isExistsUsernameByUsername(usuario)) {
+			printInfo("Creating user ADMIN...");
+			Rol r = new Rol();
+			r.setId_rol(1L);
+			r.setNombre("ADMIN"); 		
+			addRol(r);
+			
+			Rol r2 = new Rol();
+			r2.setId_rol(2L);
+			r2.setNombre("USER"); 
+			addRol(r2);
 			usuario.setNombre("Admin");
 			usuario.setApellido("Admin");
 			usuario.setPassword("root");
@@ -61,6 +76,14 @@ public class BackendNibApplication implements CommandLineRunner {
 		if(!rolService.isExistsRolByName(rol)) {
 			rolService.saveRol(rol);
 		}
+	}
+	
+	public void printInfo(String str) {
+	    String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+	    String PURPLE_BOLD = "\033[1;35m"; // PURPLE
+	    String RESET = "\033[0m"; // RESET
+	    String date = java.time.Clock.systemUTC().instant().toString();
+		System.out.println(date.substring(0, date.length()-1)+"  "+YELLOW_BOLD+"[NIB] "+PURPLE_BOLD+"    --- "+RESET + str);
 	}
 
 }
