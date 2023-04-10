3a106d5e79d3ced8fee9f7bc3b69500c5ed87eee
Clara Bujeda <95882405+CBujeda@users.noreply.github.com>
2023-04-10 10:55:10 +0200

    find by username in dao

--- PROYECT NIB/Backend_NIB/src/main/java/com/nib/app/model/dao/usuario/UsuarioDAO.java
+++ PROYECT NIB/Backend_NIB/src/main/java/com/nib/app/model/dao/usuario/UsuarioDAO.java
@@ -1,9 +1,11 @@
 package com.nib.app.model.dao.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.nib.app.model.entity.user.Usuario;
 
+@Repository
 public interface UsuarioDAO extends JpaRepository<Usuario, Long> {
 
 	public Usuario findByUsername(String username);
