9de2d84c20e45cf8416c813809e0e1f44b328037
Clara Bujeda <95882405+CBujeda@users.noreply.github.com>
2023-05-08 17:00:00 +0200

    DAOS

--- PROYECT NIB/Backend_NIB/src/main/java/com/nib/app/model/dao/usuario/TagDAO.java
+++ PROYECT NIB/Backend_NIB/src/main/java/com/nib/app/model/dao/usuario/TagDAO.java
@@ -1,6 +1,7 @@
 package com.nib.app.model.dao.usuario;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +17,6 @@ public interface TagDAO extends JpaRepository<Tag, Long>{
 			+ "ORDER BY RAND ( )  "
 			+ "LIMIT ?1 ")
 	public List<Tag> getRandomerTag(int limit);
+	
+	public Optional<Tag> findByName(String name);
 }
--- PROYECT NIB/Backend_NIB/src/main/java/com/nib/app/model/dao/usuario/UsuarioDAO.java
+++ PROYECT NIB/Backend_NIB/src/main/java/com/nib/app/model/dao/usuario/UsuarioDAO.java
@@ -1,5 +1,7 @@
 package com.nib.app.model.dao.usuario;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,9 @@ import com.nib.app.model.entity.user.Usuario;
 public interface UsuarioDAO extends JpaRepository<Usuario, Long> {
 
 	public Usuario findByUsername(String username);
+	
+	//@Query(value = "SELECT * FROM usuario u where u.token = ?1 ;")
+	//public List<Usuario> findByToken(String token);
+	
+	public Optional<Usuario> findByToken(String token);
 }
