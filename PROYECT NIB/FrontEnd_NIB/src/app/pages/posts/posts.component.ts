25d392098c58ced213743cdec2ae724788bcb7da
Clara Bujeda <95882405+CBujeda@users.noreply.github.com>
2023-05-08 17:09:01 +0200

    Muestra de posts

--- PROYECT NIB/FrontEnd_NIB/src/app/pages/posts/posts.component.ts
+++ PROYECT NIB/FrontEnd_NIB/src/app/pages/posts/posts.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PostsService } from '../../services/posts.service';
-
+import { backserv } from 'config';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -13,25 +14,51 @@ export class PostsComponent {
   public size = 18;
   public order = "date";
   public asc = true;
-
+  public imgApi = backserv.secure+"://"+backserv.ipnibbackserver+":"+backserv.portnibbackserver+"/api/image/get/";
+  public searchText = "";
 
   constructor(private postservice:PostsService){
     this.cargar_posts();
-    for(let i = 0; i < 20; i++){
-      this.posts.push("");
-    }
-
   }
 
   cargar_posts(){
+    this.posts = [];
     this.postservice.postsPages(this.page,this.size,this.order,this.asc).subscribe(
       data => {
         console.log(data);
+        let posts = data.content
+        for (let i = 0; i < posts.length; i++) {
+          console.log();
+          this.posts.push(posts[i].id_post);
+        }
       },
       err =>{
         console.log(err.error());
       }
     );
+  }
+
+  //Searching
+  submitLogin(form:NgForm) {
+    let searchdata = form.value.searchdata;
+    console.log(searchdata);
+    if(searchdata.length >= 1) {
+
+
+    }
+  }
+
+
+
+  nextPage(){
+    this.page = this.page + 1;
+    this.cargar_posts();
+  }
+  backPage(){
+    if(this.page != 0){
+      this.page = this.page - 1;
+      this.cargar_posts();
+    }
   }
 
 }
