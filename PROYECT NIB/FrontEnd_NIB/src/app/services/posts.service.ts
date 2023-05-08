e849c6e8a5b3d6e9fa8e37664d9086f80c51c5a3
Clara Bujeda <95882405+CBujeda@users.noreply.github.com>
2023-05-08 17:06:49 +0200

    Servicio de posts

--- PROYECT NIB/FrontEnd_NIB/src/app/services/posts.service.ts
+++ PROYECT NIB/FrontEnd_NIB/src/app/services/posts.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { backserv } from 'config';
 import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ import { Observable } from 'rxjs';
 export class PostsService {
 
   constructor(private httpClient: HttpClient) { } //: Observable<Any>
+
   public postsPages(page:number,size:number,order:string, asc: boolean){
     let apiPostsPageURL = "http://"+backserv.ipnibbackserver+":"+backserv.portnibbackserver
                   +"/api/public/posts/postsLimited?";
@@ -18,5 +20,13 @@ export class PostsService {
   public getContPosts(){
     let apiPostsCont = "http://" + backserv.ipnibbackserver + ":" + backserv.portnibbackserver + "/api/public/posts/count";
     return this.httpClient.get<any>(apiPostsCont)
+  }
+
+  public sendNewPost(title: string, tags: string, file: String, token: string){
+    console.log("Send")
+    const json = { title: title, tags: tags,base64Img:file, token: token};
+    let apiPostsSave = "http://" + backserv.ipnibbackserver + ":" + backserv.portnibbackserver + "/api/private/post/postnew";
+    console.log(apiPostsSave)
+    return this.httpClient.post<any>(apiPostsSave, json);//, { headers: headers }
   }
 }
