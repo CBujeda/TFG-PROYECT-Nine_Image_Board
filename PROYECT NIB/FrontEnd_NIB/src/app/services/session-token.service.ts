a2e8dc6f41d19662455405f129875caa798418a2
Clara Bujeda <95882405+CBujeda@users.noreply.github.com>
2023-05-08 17:06:41 +0200

    Sistema de seguridad de tokens

---
+++ PROYECT NIB/FrontEnd_NIB/src/app/services/session-token.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { backserv } from 'config';
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionTokenService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  private key: string = "nib_token";
+
+  //----------------TOKEN STORAGE ----------------
+
+  public writeToken(token: string){
+    //sessionStorage.setItem(this.key, token);
+    localStorage.setItem(this.key, token);
+  }
+  public readToken(){
+    //const miDato = sessionStorage.getItem(this.key) || null;
+    const miDato = localStorage.getItem(this.key) || null;
+    return miDato;
+  }
+
+  // SOLICITING TOKEN TO BACKEND ----------------
+
+  public makeLogin(json: any){
+    let apiPostsCont = "http://" + backserv.ipnibbackserver + ":" + backserv.portnibbackserver + "/api/auth/login";
+    return this.httpClient.post<any>(apiPostsCont,json)
+  }
+
+  // Verify token to backend     ----------------
+
+  public verifyToken(json: any){
+    let apiPostsCont = "http://" + backserv.ipnibbackserver + ":" + backserv.portnibbackserver + "/api/auth/verifyToken";
+    return this.httpClient.post<any>(apiPostsCont,json)
+  }
+
+  public infoUserToken(json: any){
+    let apiPostsCont = "http://" + backserv.ipnibbackserver + ":" + backserv.portnibbackserver + "/api/users/get";
+    return this.httpClient.post<any>(apiPostsCont,json)
+  }
+}
